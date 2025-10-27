d19a0e7b274455623d750bfa5dc000bc6e1b60e5
Daksh kweera <kweera2005@gmail.com>
2025-10-28 02:03:55 +0530

    render1

--- backend/app.js
+++ backend/app.js
@@ -7,8 +7,12 @@ import cors from "cors";
 const app = express();
 // app.use(cors());
 app.use(cors({
-  origin: 'http://localhost:5173'
+  origin: process.env.NODE_ENV === 'production'
+    ? 'https://dhanalysis.vercel.app'
+    : 'http://localhost:5173'
 }));
+
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
